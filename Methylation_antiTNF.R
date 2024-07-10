3144513077cab254b1a3bdb6657d1428ab3cd2c6
Grazi Credidio <grazi.credidio@gmail.com>
2024-07-10 19:37:50 +0200

    Methylation correlation for antiTNF analysis: fixed paths, cleaned code. GO analysis removed > will be done in another script

--- Methylation_antiTNF.R
+++ Methylation_antiTNF.R
@@ -1,40 +1,47 @@
-# EZE Cohort - therapy signatures
-# Methylation data integration with transcriptome
-# Anti-TNF x No Biologics - remitters + crp significant genes
+# EZE cohort: Therapy Signatures
+  # DNA methylation-linked genes: Correlation between DNAm intensity of sites located 5000 bp up- and downstream of the TSS DEGs
+  # Anti-TNF x No Biologics 
+  # Author: Graziella Credidio
 
 rm(list = ls())
 
-
-library(topGO)
+# Loading packages ----
 library(org.Hs.eg.db)
-library(GO.db)
-library(ComplexHeatmap)
-library(RColorBrewer)
-library(plyr)
 library(tibble)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 
-
-
-#Correlation analysis between transcription and methylation--------
-
-#Read and filter methylation data for sites
-methylation_data <- read.csv("Methylation_data/m_values_hg38_round.txt", header = TRUE, row.names = NULL, 
+# Loading data ----
+# Methylation data
+methylation_data <- read.csv("Raw_tables/methylation/m_values_hg38_round.txt", header = TRUE, row.names = NULL, 
                              sep = '\t')
+meth_sample_info <- read.csv("Raw_tables/methylation/DZHK_EZE_epicarray_metadata_with_remission_status_090223.csv",
+                             header = TRUE, sep = ',')
+gene_meth_sites <- read.csv("Raw_tables/methylation/gene_meth_sites_5000bp.txt", sep = '\t')
 
+# Normalized gene counts and coldata
+transcriptome_data <- read.table("Cleaned_tables/models/antiTNF/normalized_counts_antiTNF.txt", sep = "\t")
+transcriptome_info_file <- read.table("Cleaned_tables/models/antiTNF/EZECohort_coldata_antiTNF.txt", sep = "\t")
+
+# DEGs
+degs <- read.table("Output_files/Maaslin2/significant_results/maaslin2_significant_results_antiTNF_vs_noBio.txt", sep = '\t')
+deg_gene_list <- degs$feature
+
+# To add gene names
+ensg2gene <- read.table("Cleaned_tables/ensg2gene_EZE.txt", header = TRUE, sep = ",")
+unique_ensg2gene <- subset(ensg2gene, duplicated(ensg2gene$ensembl_gene_id) == FALSE)
+rownames(unique_ensg2gene) <- unique_ensg2gene$ensembl_gene_id
+
+# Methylation data cleaning ----
+# Removing NAs
 which(is.na(methylation_data$row.names)) #10359 = NA
 methylation_data <- methylation_data[-10359,]
 
-meth_sample_info <- read.csv("Methylation_data/DZHK_EZE_epicarray_metadata_with_remission_status_090223.csv",
-                             header = TRUE, sep = ',')
-
-
+# Filter methylation data for sites in meth_sample_info
 methylation_data_filtered <- methylation_data[, colnames(methylation_data) %in% paste('X', as.character(meth_sample_info$Sample_ID), sep = '')]
 rownames(methylation_data_filtered) <- methylation_data$row.names
 names(methylation_data_filtered) <- gsub(pattern= "X", replacement = "", x=names(methylation_data_filtered))
 
-#Matching order of sample info and methylation data filtered
+# Matching order of sample info and methylation data filtered
 meth_sample_info_filterd <- meth_sample_info[meth_sample_info$Sample_ID %in% colnames(methylation_data_filtered),]
 idx <- match(colnames(methylation_data_filtered), meth_sample_info_filterd$Sample_ID) 
 ord.meth_sample_info_filtered <- meth_sample_info_filterd[idx, ]
@@ -42,17 +49,10 @@ ord.meth_sample_info_filtered <- meth_sample_info_filterd[idx, ]
 all(colnames(methylation_data_filtered) %in% ord.meth_sample_info_filtered$Sample_ID)
 all(colnames(methylation_data_filtered) == ord.meth_sample_info_filtered$Sample_ID)
 
-# replacing sample_id by Study ID
+# Replacing sample_id by Study ID
 colnames(methylation_data_filtered) <- ord.meth_sample_info_filtered$Study.ID
 
-
-#Read and filter transcriptome data for genes
-transcriptome_data <- read.table("Output_files/DESeq2/Maaslin2/Remission_crp/DESeq2_normalized_antiTNF_R_crp_04.05.txt", sep = "\t")
-transcriptome_info_file <- read.table("Output_files/Maaslin2/Tables/Remission_crp/coldata_maaslin2_antiTNF_R_crp_04.05.txt", sep = "\t") #coldata
-
-transcriptome_data <- transcriptome_data[, as.character(transcriptome_info_file$sample_id)] #normalized counts filtered by aza patients
-
-#Matching order of sample info and transcriptome sig
+# Matching order of samples in transcriptome_info_file and transcriptome_data
 idx2 <- match(colnames(transcriptome_data), transcriptome_info_file$sample_id) 
 ord.transcriptome_info_file <- transcriptome_info_file[idx2, ]
 
@@ -61,20 +61,16 @@ all(colnames(transcriptome_data) == ord.transcriptome_info_file$sample_id)
 
 colnames(transcriptome_data) <- ord.transcriptome_info_file$study_id
 
+# Matching samples in transcriptome and methylome data
 common_samples <- intersect(colnames(transcriptome_data), colnames(methylation_data_filtered))
 transcriptome_data <- transcriptome_data[, common_samples]
 methylation_data_filtered <- methylation_data_filtered[, common_samples]
 
-#Read list of differentially expressed genes 
-deg_gene_list <- read.table("Output_files/Maaslin2/Results/Remission_crp/significant_results/maaslin2_significant_results_antiTNF_vs_noBio_R_crp.txt",
-                            sep = '\t')
-deg_gene_list <- deg_gene_list$feature
-
-#Read and filter gene and linked methylation sites info, remove sites on sex chromosomes
-gene_meth_sites <- read.csv("Methylation_data/gene_meth_sites_5000bp.txt", sep = '\t')
+# Read and filter gene and linked methylation sites info, remove sites on sex/mythoochondria chromosomes
 gene_meth_sites <- gene_meth_sites[,-9] #removing column X
 
-gene_meth_sites <- subset(gene_meth_sites, gene_meth_sites$Chr != 'chrX' & gene_meth_sites$Chr != 'chrY' & 
+gene_meth_sites <- subset(gene_meth_sites, gene_meth_sites$Chr != 'chrX' & 
+                            gene_meth_sites$Chr != 'chrY' & 
                             gene_meth_sites$Chr != 'chrM')
 rownames(gene_meth_sites) <- gene_meth_sites$Gene_id
 
@@ -83,14 +79,11 @@ deg_gene_list <- intersect(deg_gene_list, rownames(gene_meth_sites))
 deg_gene_meth_sites <- gene_meth_sites[as.character(deg_gene_list), ]
 deg_gene_meth_sites <- subset(deg_gene_meth_sites, deg_gene_meth_sites$no_of_meth_sites > 0)
 
-
-#Calculate correlation coefficient between the gene expression of each gene and the methylation intensity of each of its link methylated sites
-#Calculate FDR using a permutation approach
+# Calculate correlation coefficient between the DEGs gene expression and the methylation intensity of TSS methylation sites----
 deg_gene_meth_sites$meth_sites <- as.character(deg_gene_meth_sites$meth_sites)
 deg_gene_meth_sites$no_of_meth_sites <- as.numeric(deg_gene_meth_sites$no_of_meth_sites)
 gene_meth_site_correlation <- matrix(nrow = sum(deg_gene_meth_sites$no_of_meth_sites), ncol = 5)
 rownum = 1
-
 
 for (i in 1:nrow(deg_gene_meth_sites)) {
   meth_sites <- strsplit(deg_gene_meth_sites[i,8], split = ';')
@@ -130,170 +123,12 @@ gene_meth_site_correlation$FDR <- as.numeric(as.character(gene_meth_site_correla
 write.table(gene_meth_site_correlation, file = "Output_files/Methylation/antiTNF/antiTNF_DEG_meth_site_correlation_5000bp_1000rep.txt", quote = FALSE,
             sep = '\t')
 
-
-#Extract significant DEGs  ----
-#gene_meth_site_correlation <- read.table("Output_files/Methylation/antiTNF_DEG_meth_site_correlation_5000bp_1000rep.txt", sep = '\t')
-
-sig_gene_meth_site_correlation <- gene_meth_site_correlation %>% #118
+# Extract significant DNAm linked DEGs  ----
+sig_gene_meth_site_correlation <- gene_meth_site_correlation %>% 
   filter(FDR < 0.05)
 
-# Add gene names
-ensg2gene <- read.table("Cleaned_tables/ensg2gene_EZE.txt", header = TRUE, sep = ",")
-unique_ensg2gene <- subset(ensg2gene, duplicated(ensg2gene$ensembl_gene_id) == FALSE)
-rownames(unique_ensg2gene) <- unique_ensg2gene$ensembl_gene_id
-
-# save
+# Save significantly correlated sites file
 sig_gene_meth_site_correlation$Gene_name <- unique_ensg2gene[as.character(sig_gene_meth_site_correlation$Gene), "hgnc_symbol"]
+
 write.table(sig_gene_meth_site_correlation, file = "Output_files/Methylation/antiTNF/antiTNF_significant_DEG_meth_site_correlation_5000bp_1000rep.txt", quote = FALSE,
             sep = '\t')
-
-
-# split into up and downregulated significant genes from correlation
-sig_genes <- read.table("Output_files/Maaslin2/Results/Remission_crp/significant_results/maaslin2_significant_results_antiTNF_vs_noBio_R_crp.txt",
-                        sep = '\t')
-
-up_sig_genes <- sig_genes %>% 
-  filter(coef > 0) %>% 
-  pull(feature)
-
-up_sig_gene_meth_site_correlation <- sig_gene_meth_site_correlation[sig_gene_meth_site_correlation$Gene %in% up_sig_genes,]
-write.table(up_sig_gene_meth_site_correlation, file = "Output_files/Methylation/antiTNF/antiTNF_up_significant_DEG_meth_site_correlation_5000bp_1000rep.txt", quote = FALSE,
-            sep = '\t')
-
-
-down_sig_genes <- sig_genes %>% 
-  filter(coef < 0) %>% 
-  pull(feature)
-
-down_sig_gene_meth_site_correlation <- sig_gene_meth_site_correlation[sig_gene_meth_site_correlation$Gene %in% down_sig_genes,]
-write.table(down_sig_gene_meth_site_correlation, file = "Output_files/Methylation/antiTNF/antiTNF_down_significant_DEG_meth_site_correlation_5000bp_1000rep.txt", quote = FALSE,
-            sep = '\t')
-
-
-#GO term enrichment analysis --------
-#loading files
-sig_gene_meth_site_correlation <- read.table("Output_files/Methylation/antiTNF/antiTNF_significant_DEG_meth_site_correlation_5000bp_1000rep.txt",
-                                             sep = '\t')
-up_sig_gene_meth_site_correlation <- read.table("Output_files/Methylation/antiTNF/antiTNF_up_significant_DEG_meth_site_correlation_5000bp_1000rep.txt",
-                                                sep = '\t')
-down_sig_gene_meth_site_correlation <- read.table("Output_files/Methylation/antiTNF/antiTNF_down_significant_DEG_meth_site_correlation_5000bp_1000rep.txt", 
-                                                  sep = '\t')
-transcriptome_data <- read.table("Output_files/DESeq2/Maaslin2/Remission_crp/DESeq2_normalized_antiTNF_R_crp_04.05.txt", sep = "\t")
-
-# setting genes to GO analysis 
-upgenes <- unique(up_sig_gene_meth_site_correlation$Gene)
-downgenes <- unique(down_sig_gene_meth_site_correlation$Gene)
-all_genes <- rownames(transcriptome_data)
-
-#Perform GO term analysis for upregulated and downregulated genes
-
-direction <- c("up")
-gene_set <- upgenes
-
-direction <- c("down")
-gene_set <- downgenes
-
-for (i in 1:length(direction)) {
-  #Define background set and test set
-  inUniverse <- all_genes %in% all_genes
-  inSelection <- all_genes %in% as.vector(gene_set)
-  alg <- factor( as.integer( inSelection[inUniverse] ) )
-  names(alg) <- all_genes[inUniverse]
-  
-  #Run topGO analysis
-  tgd <- new( "topGOdata", ontology="BP", allGenes = alg, nodeSize=5,
-              annot=annFUN.org, mapping="org.Hs.eg.db", ID = "ensembl" )
-  
-  resultTopGO.elim <- runTest(tgd, algorithm = "elim", statistic = "Fisher" )
-  resultTopGO.classic <- runTest(tgd, algorithm = "classic", statistic = "Fisher" )
-  
-  go_results <- GenTable(tgd, Fisher.elim = resultTopGO.elim, 
-                         Fisher.classic = resultTopGO.classic,
-                         orderBy = "Fisher.elim", numChar=1000, topNodes = length(nodes(graph(tgd))))
-  
-  #Extract significant results and add genes contributing to each GO term
-  go_results <- subset(go_results, go_results$Fisher.elim < 0.05)
-  for(i in 1:nrow(go_results)){
-    go_id <- as.vector(go_results[i,1])
-    alleges <- get(go_id, org.Hs.egGO2ALLEGS)
-    genes <- unlist(mget(alleges, org.Hs.egENSEMBL))
-    genes_in_cat <- intersect(as.vector(genes), as.vector(gene_set)) #changed
-    gene_sym_in_cat <- as.vector(unlist(mget(unlist(mget(genes_in_cat, org.Hs.egENSEMBL2EG)), org.Hs.egSYMBOL)))
-    gene_sym_in_cat_str <- ""
-    
-    if(length(genes_in_cat) > 0){
-      for(j in 1:length(gene_sym_in_cat)){
-        gene_sym_in_cat_str <- paste(gene_sym_in_cat_str, gene_sym_in_cat[j], sep = ',')
-      }
-    }
-    if (gene_sym_in_cat_str == ""){
-      gene_sym_in_cat_str <- NA
-    }
-    go_results$Genes[i] <- gene_sym_in_cat_str
-  }
-  
-  go_results <- go_results %>% 
-    mutate(geneRatio = go_results[,4]/go_results[,3]) %>% 
-    relocate(geneRatio, .after = Expected)
-  
-  
-  write.table(go_results, paste("Output_files/Methylation/antiTNF/GO/antiTNF_correlated_", direction, "_DEGs_GO.txt", sep = ''), 
-              sep = '\t', quote = FALSE)
-  
-}
-
-
-up_go_result <- read.csv("Output_files/Methylation/antiTNF/GO/antiTNF_correlated_up_DEGs_GO.txt", sep = '\t')
-down_go_result <- read.csv("Output_files/Methylation/antiTNF/GO/antiTNF_correlated_down_DEGs_GO.txt", sep = '\t')
-
-#Plot selected GO terms enriched in DEGs 
-terms <- c("immunoglobulin production", "adaptive immune response", "cartilage development", "regulation of cell cycle G1/S phase transition",
-           "interleukin-1-mediated signaling pathway", "positive regulation of chromosome condensation",
-           
-           "DNA replication-dependent chromatin assembly", "response to osmotic stress", "positive regulation of signal transduction", 
-           "regulation of mRNA stability")
-
-
-plot_up_go_result <- up_go_result %>% 
-  mutate(plot = ifelse(Term %in% terms, "Yes", "No")) %>% 
-  filter(plot == "Yes") %>% 
-  mutate(direction = "Upregulated")
-
-plot_down_go_result <- down_go_result %>% 
-  mutate(plot = ifelse(Term %in% terms, "Yes", "No")) %>% 
-  filter(plot == "Yes") %>% 
-  mutate(direction = "Downregulated")
-
-selected_GO <- rbind(plot_up_go_result, plot_down_go_result)
-selected_GO$Term <- Term(as.vector(selected_GO$GO.ID))
-selected_GO$Term <- factor(selected_GO$Term, levels = rev(selected_GO$Term)) # fixes order
-
-
-selected_GO$Genes <- sub('.', '', selected_GO$Genes)
-
-selected_GO %>%
-  dplyr::select(GO.ID, Term, Annotated, Significant, geneRatio, Fisher.elim, Genes, direction) %>% 
-  kbl() %>%
-  kable_styling()
-
-
-pdf("Output_files/Methylation/antiTNF/GO/dotplot_GO_BP_antiTNF_R_crp_data_integration.pdf", width = 9, height = 6)
-dot_plot <- ggplot(selected_GO, aes(x = direction, y= Term, size = geneRatio, color = Fisher.elim)) +
-  geom_point() +
-  scale_colour_gradient(low = "#990000", high = "#FF9999") +
-  #scale_size_continuous(range=c(3,10)) +
-  xlab('') + ylab('') +
-  labs(
-    title = "GO BP AntiTNF x No Biologics",
-    subtitle = "Transcriptome-methylome integration",
-    caption = paste0("Produced on ", Sys.time()),
-    color = "p-value",
-    size = "Gene ratio") +
-  theme_bw() + #base_size = 20
-  theme(
-    axis.text.y = element_text(hjust = 1, size=12, color = "black"), 
-    axis.text.x = element_text(size=14, color = "black", angle = 45, hjust = 1),
-    axis.title = element_text(size = 20)
-  )
-dot_plot
-dev.off()
