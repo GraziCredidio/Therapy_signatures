dff9dfe2e736d22f64d96557c76b7f8b2825d9f6
Grazi Credidio <grazi.credidio@gmail.com>
2024-07-15 20:00:42 +0200

    Fixed paths and coldatas. Output > figure 1

--- Plots_EZEcohort_patientDistribution_Remitters.R
+++ Plots_EZEcohort_patientDistribution_Remitters.R
@@ -1,126 +1,192 @@
-# EZE cohort
-# Bar plot with number of patients in each comparison by diagnosis (remitters + crp)
+# EZE cohort: Therapy Signatures
+  # Number of patients with inactive disease by treatment and diagnosis
+  # Figure 1 
+  # Author: Graziella Credidio
 
-graphics.off()
 rm(list = ls())
 
-setwd("C:\\Documents/Masters thesis/EZE_cohort") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort") #PC
-
+# Loading packages ----
 library(RColorBrewer)
 library(tidyverse)
 
 # Loading files ----
-#inactive disease patients
-setwd("C:/Documents/Masters thesis/EZE_cohort/Output_files/Maaslin2/Tables/Remission_crp") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort/Output_files/Maaslin2/Tables/Remission_crp") #pc
+aza_noSyst_coldata <- read.table("Cleaned_tables/models/aza/EZECohort_coldata_aza.txt", sep = "\t")
+pred_noSyst_coldata <- read.table("Cleaned_tables/models/pred/EZECohort_coldata_pred.txt", sep = "\t")
+antiTNF_noBio_coldata <- read.table("Cleaned_tables/models/antiTNF/EZECohort_coldata_antiTNF.txt", sep = "\t")
 
-files_R = list.files(pattern="*.txt")
-coldata_R = lapply(files_R, read.delim)
+coldata <- read.table("Cleaned_tables/EZECohort_coldata_clean_ord.txt")
 
-coldata_noBio <- coldata_R[[4]]
-coldata_aza_noSyst <- coldata_R[[5]]
-coldata_pred_noSyst <- coldata_R[[6]]
+# coldata with only inactive disease patients----
+coldata <- coldata %>% 
+  filter(remission == "R") %>% 
+  filter(crp < 5)
 
-# all patients
-setwd("C:/Documents/Masters thesis/EZE_cohort/Output_files/Maaslin2/Tables") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort/Output_files/Maaslin2/Tables") #pc
+# preprocessing existing coldatas ----
+aza_noSyst_coldata <- aza_noSyst_coldata %>% 
+  mutate(Comparison = "aza x no syst",
+         Treatment = case_when(
+           aza_vs_noSyst == 'aza' ~ "Aza",
+           aza_vs_noSyst == "no_syst" ~ "No syst (aza)")) %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-files = list.files(pattern="*.txt")
-coldata = lapply(files, read.delim)
+pred_noSyst_coldata <- pred_noSyst_coldata %>% 
+  mutate(Comparison = "pred x no syst",
+         Treatment = case_when(
+           pred_vs_noSyst == "pred" ~ "Pred",
+           pred_vs_noSyst == "no_syst" ~ "No syst (pred)")) %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-coldata_nonTNF <- coldata[[3]]
-coldata_noAza <- coldata[[4]]
-coldata_noMtx <- coldata[[6]]
-coldata_mtx_noSyst <- coldata[[7]]
-coldata_noPred <- coldata[[8]]
+antiTNF_noBio_coldata <- antiTNF_noBio_coldata %>% 
+  mutate(Comparison = "anti-TNF",
+         Treatment = case_when(
+           antiTNF_vs_noBiologics == "anti_tnf" ~ "anti-TNF",
+           antiTNF_vs_noBiologics == "no_biologics" ~ 'No biologics')) %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
+  
+# Creating coldata for other models ----
+# Aza x no aza
+aza_noAza_coldata <- coldata %>%
+  filter(diagnosis_class == "CD" | diagnosis_class == "UC") %>% # only IBD patients
+  mutate(aza_vs_noAza = case_when(
+    (Azathioprin == "1") ~ "Aza",
+    (Prednisolon == "1" |
+       MTX == "1" |
+       X6.MP == "1" |
+       Sulfasalazin == "1" |
+       Chloroquin == "1" |
+       Hydroxychloroquin == "1" |
+       Leflunomid == "1" |
+       Ciclosporin == "1" |
+       Mycophenolat.Mofetil == "1" |
+       Cyclophosphamid == "1" |
+       Tacrolimus == "1" |
+       Apremilast == "1" |
+       Mepacrine == "1" |
+       No_syst == "1") ~ "No aza")) %>% 
+  relocate(aza_vs_noAza, .after = No_syst) %>% 
+  mutate(Comparison = "aza x no aza",
+       Treatment = aza_vs_noAza) %>% 
+  filter(aza_vs_noAza == "No aza") %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-# filtering to inactive + crp
-setwd("C:\\Documents/Masters thesis/EZE_cohort") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort") #PC
-
-coldata_nonTNF <- coldata_nonTNF %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noAza <- coldata_noAza %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noMtx <- coldata_noMtx %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_mtx_noSyst <- coldata_mtx_noSyst %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noPred <- coldata_noPred %>% 
-  filter(remission == "R" & crp < 5)
-
-
-# Pre processing ----
-all_diagnosis <- data.frame()
-
-diag_noBio <- coldata_noBio %>% 
-  mutate(Comparison = "anti-TNF x no biologics",
-         Treatment = antiTNF_vs_noBiologics,
-         biologics_TNF = antiTNF_vs_noBiologics) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_nonTNF <- coldata_nonTNF %>% 
-  mutate(Comparison = "anti-TNF x non-anti-TNF",
-         Treatment = antiTNF_vs_nonAntiTNF,
-         biologics_TNF = antiTNF_vs_nonAntiTNF ) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF) 
-
-
-diag_noPred <- coldata_noPred %>% 
+# Pred x no pred
+pred_noPred_coldata <- coldata %>% 
+  mutate(pred_vs_noPred = case_when(
+    (Prednisolon == "1") ~ "Pred",
+    (Azathioprin == "1" |
+       MTX == "1" |
+       X6.MP == "1" |
+       Sulfasalazin == "1" |
+       Chloroquin == "1" |
+       Hydroxychloroquin == "1" |
+       Leflunomid == "1" |
+       Ciclosporin == "1" |
+       Mycophenolat.Mofetil == "1" |
+       Cyclophosphamid == "1" |
+       Tacrolimus == "1" |
+       Apremilast == "1" |
+       Mepacrine == "1" |
+       No_syst == "1") ~ "No pred")) %>% 
+  relocate(pred_vs_noPred, .after = No_syst) %>% 
+  filter(!(diagnosis_class == "Pso")) %>%  # Exclude Pso patients
   mutate(Comparison = "pred x no pred",
          Treatment = pred_vs_noPred) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
+  filter(pred_vs_noPred == "No pred") %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-diag_pred_noSyst <- coldata_pred_noSyst %>% 
-  mutate(Comparison = "pred x no syst",
-         Treatment = pred_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
+# Anti-TNF x non-anti-TNF
+antiTNF_nonAntiTF_coldata <- coldata %>% 
+  filter(biologics_TNF == "anti_tnf" | biologics_TNF == "non_anti_tnf") %>%
+  dplyr::rename(antiTNF_vs_nonAntiTNF = biologics_TNF) %>% 
+  mutate(Comparison = "anti-TNF x non-anti-TNF",
+         Treatment = case_when(
+           antiTNF_vs_nonAntiTNF == "anti_tnf" ~ "anti-TNF",
+           antiTNF_vs_nonAntiTNF == "non_anti_tnf" ~ "Non-anti-TNF")) %>% 
+  filter(antiTNF_vs_nonAntiTNF == "non_anti_tnf") %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
+# Mtx x no Syst
+mtx_noSyst_coldata <- coldata %>% 
+  mutate(mtx_vs_noSyst = case_when(
+    (MTX == "1")~ "mtx",
+    (No_syst == "1") ~ "no_syst")) %>% 
+  relocate(mtx_vs_noSyst, .after = No_syst) %>% 
+  mutate(Comparison = "MTX x no syst",
+         Treatment = case_when(
+           mtx_vs_noSyst == 'mtx' ~ "MTX",
+           mtx_vs_noSyst == 'no_syst' ~ "No syst (MTX)")) %>% 
+  filter(diagnosis_class == "RA" | diagnosis_class == "PsA") %>% # only RA and PsA patients
+  filter(!(is.na(mtx_vs_noSyst))) %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-diag_aza_noAza <- coldata_noAza %>% 
-  mutate(Comparison = "aza x no aza",
-         Treatment = aza_vs_noAza) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_aza_noSyst <- coldata_aza_noSyst %>% 
-  mutate(Comparison = "aza x no syst",
-         Treatment = aza_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-
-diag_mtx_noMtx <- coldata_noMtx %>% 
+# Mtx x no mtx
+mtx_noMtx_coldata <- coldata %>% 
+  mutate(mtx_vs_noMtx = case_when(
+    (MTX == "1") ~ "mtx",
+    (Prednisolon == "1" |
+       Azathioprin == "1" |
+       X6.MP == "1" |
+       Sulfasalazin == "1" |
+       Chloroquin == "1" |
+       Hydroxychloroquin == "1" |
+       Leflunomid == "1" |
+       Ciclosporin == "1" |
+       Mycophenolat.Mofetil == "1" |
+       Cyclophosphamid == "1" |
+       Tacrolimus == "1" |
+       Apremilast == "1" |
+       Mepacrine == "1" |
+       No_syst == "1") ~ "no_mtx")) %>% 
+  relocate(mtx_vs_noMtx, .after = No_syst) %>% 
   mutate(Comparison = "MTX x no MTX",
-         Treatment = mtx_vs_noMtx) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
+         Treatment = case_when(
+           mtx_vs_noMtx == "mtx" ~ "MTX",
+           mtx_vs_noMtx == "no_mtx" ~ "No MTX")) %>% 
+  filter(diagnosis_class == "RA" | diagnosis_class == "PsA") %>% # only RA and PsA patients
+  filter(!(is.na(mtx_vs_noMtx))) %>% 
+  filter(mtx_vs_noMtx == "no_mtx") %>% 
+  dplyr::select(study_id, diagnosis_class, Treatment, Comparison)
 
-diag_mtx_noSyst <- coldata_mtx_noSyst %>% 
-  mutate(Comparison = "mtx x no syst",
-         Treatment = mtx_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-all_diagnosis <- rbind(diag_noBio, diag_nonTNF, diag_noPred, diag_pred_noSyst,
-                       diag_aza_noAza,diag_aza_noSyst,diag_mtx_noMtx, diag_mtx_noSyst) 
-
-all_diagnosis$Treatment <- as.factor(all_diagnosis$Treatment)
-
-
-all_diagnosis<- all_diagnosis %>% 
-  mutate(Treatment_binary = case_when(
-    (Treatment == "anti_tnf" | Treatment == "pred" | Treatment == "aza" | Treatment == "mtx") ~ "Yes",
-    (Treatment == "no_biologics" | Treatment == "non_anti_tnf" | Treatment == "no_mtx" |
-       Treatment == "no_syst" | Treatment == "no_aza" | Treatment == "no_pred") ~ "No"
-  ))
+# Pre processing ----
+all_coldata <- rbind(antiTNF_noBio_coldata, antiTNF_nonAntiTF_coldata, 
+                     pred_noSyst_coldata, pred_noPred_coldata,
+                     aza_noSyst_coldata, aza_noAza_coldata,
+                     mtx_noSyst_coldata, mtx_noMtx_coldata)
 
 
-# Plot by diagnosis ----
+# Plots ----
+# Figure 1
+breaks <- seq(from = 0, to = 200, by = 25)
+plot_remitters_patients_treatments <- ggplot(all_coldata, aes(Treatment, fill = diagnosis_class)) +
+  geom_bar() + theme_bw() +
+  geom_text(aes(label = after_stat(count)), stat = "count", hjust = 0.5, position = position_stack(vjust = 0.5), colour = "#808080", size = 5) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 14),
+    axis.title.x=element_blank(),
+    axis.title.y =element_blank(),
+    title = element_text(size = 24),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 14),
+    legend.position = "bottom"
+  ) +
+  scale_y_continuous(breaks = breaks) +
+  labs(fill= 'Diagnosis',
+       title = "Inactive disease patients") +
+  scale_fill_brewer(type = "qual", palette = "Pastel2") +
+  scale_x_discrete(limits= c("anti-TNF", "No biologics", "Non-anti-TNF", "Aza",
+                            "No aza", "No syst (aza)", "MTX", "No MTX", "No syst (MTX)", 
+                            "Pred", "No pred", "No syst (pred)"))
+plot_remitters_patients_treatments 
+ggsave(plot_remitters_patients_treatments,
+       file = "Output_files/Patients_distribution/inactive_byComparison_byDiagnosis.png",
+       height = 8, width = 15, units = "in", dpi = 300)
+
+
+
+
+# Supplementary figure 1: Inactive disease patients by comparison and diagnosis
 breaks <- seq(from = 0, to = 600, by = 50)
-
-plot1 <- ggplot(all_diagnosis, aes(Comparison, fill = diagnosis_class)) +
+plot_remitters_patients_comparisons <- ggplot(all_coldata, aes(Comparison, fill = diagnosis_class)) +
   geom_bar() + theme_bw() +
   geom_text(aes(label = after_stat(count)), stat = "count", hjust = 0.5, position = position_stack(vjust = 0.5), colour = "#808080", size = 5) +
   theme(
@@ -133,188 +199,9 @@ plot1 <- ggplot(all_diagnosis, aes(Comparison, fill = diagnosis_class)) +
   ) +
   scale_y_continuous(breaks = breaks) +
   labs(fill= 'Diagnosis',
-       title = "Patients included in comparisons") +
+       title = "Inactive disease patients") +
   scale_fill_brewer(type = "qual", palette = "Pastel2")
-plot1 
-
-ggsave(plot1, file = "Output_files/Patient_distribution/R_crp/byComparison_byDiagnosis.png", height = 8, width = 15, units = "in", dpi = 300)
-
-
-
-##### Splitting by no syst/nobio and treatment
-
-graphics.off()
-rm(list = ls())
-
-setwd("C:\\Documents/Masters thesis/EZE_cohort") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort") #PC
-
-library(RColorBrewer)
-library(tidyverse)
-
-# Loading files ----
-#inactive disease patients
-setwd("C:/Documents/Masters thesis/EZE_cohort/Output_files/Maaslin2/Tables/Remission_crp") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort/Output_files/Maaslin2/Tables/Remission_crp") #pc
-
-files_R = list.files(pattern="*.txt")
-coldata_R = lapply(files_R, read.delim)
-
-coldata_noBio <- coldata_R[[4]]
-coldata_aza_noSyst <- coldata_R[[5]]
-coldata_pred_noSyst <- coldata_R[[6]]
-
-# all patients
-setwd("C:/Documents/Masters thesis/EZE_cohort/Output_files/Maaslin2/Tables") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort/Output_files/Maaslin2/Tables") #pc
-
-files = list.files(pattern="*.txt")
-coldata = lapply(files, read.delim)
-
-coldata_nonTNF <- coldata[[3]]
-coldata_noAza <- coldata[[4]]
-coldata_noMtx <- coldata[[6]]
-coldata_mtx_noSyst <- coldata[[7]]
-coldata_noPred <- coldata[[8]]
-
-# filtering to inactive + crp
-setwd("C:\\Documents/Masters thesis/EZE_cohort") #laptop
-setwd("~/Workspace/Masters-Thesis/EZE/EZE_cohort") #PC
-
-coldata_nonTNF <- coldata_nonTNF %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noAza <- coldata_noAza %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noMtx <- coldata_noMtx %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_mtx_noSyst <- coldata_mtx_noSyst %>% 
-  filter(remission == "R" & crp < 5)
-
-coldata_noPred <- coldata_noPred %>% 
-  filter(remission == "R" & crp < 5)
-
-
-# Pre processing ----
-all_diagnosis <- data.frame()
-
-diag_noBio <- coldata_noBio %>% 
-  mutate(Comparison = "anti-TNF x no biologics",
-         Treatment = antiTNF_vs_noBiologics,
-         biologics_TNF = antiTNF_vs_noBiologics) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_nonTNF <- coldata_nonTNF %>% 
-  mutate(Comparison = "anti-TNF x non-anti-TNF",
-         Treatment = antiTNF_vs_nonAntiTNF,
-         biologics_TNF = antiTNF_vs_nonAntiTNF ) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF) 
-
-
-diag_noPred <- coldata_noPred %>% 
-  mutate(Comparison = "pred x no pred",
-         Treatment = pred_vs_noPred) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_pred_noSyst <- coldata_pred_noSyst %>% 
-  mutate(Comparison = "pred x no syst",
-         Treatment = pred_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-
-diag_aza_noAza <- coldata_noAza %>% 
-  mutate(Comparison = "aza x no aza",
-         Treatment = aza_vs_noAza) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_aza_noSyst <- coldata_aza_noSyst %>% 
-  mutate(Comparison = "aza x no syst",
-         Treatment = aza_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-
-diag_mtx_noMtx <- coldata_noMtx %>% 
-  mutate(Comparison = "MTX x no MTX",
-         Treatment = mtx_vs_noMtx) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-diag_mtx_noSyst <- coldata_mtx_noSyst %>% 
-  mutate(Comparison = "mtx x no syst",
-         Treatment = mtx_vs_noSyst) %>% 
-  select(study_id, diagnosis_class, Treatment, Comparison, age_group2, biologics_TNF)
-
-all_diagnosis <- rbind(diag_noBio, diag_nonTNF, diag_noPred, diag_pred_noSyst,
-                       diag_aza_noAza,diag_aza_noSyst,diag_mtx_noMtx, diag_mtx_noSyst) 
-
-all_diagnosis$Treatment <- as.factor(all_diagnosis$Treatment)
-
-# all_diagnosis<- all_diagnosis %>% 
-#   mutate(Treatment_binary = case_when(
-#     (Treatment == "anti_tnf" | Treatment == "pred" | Treatment == "aza" | Treatment == "mtx") ~ "Yes",
-#     (Treatment == "no_biologics" | Treatment == "non_anti_tnf" | Treatment == "no_mtx" |
-#        Treatment == "no_syst" | Treatment == "no_aza" | Treatment == "no_pred") ~ "No"
-#   ))
-
-
-
-diagnosis_treatment <- all_diagnosis %>% 
-  filter(Treatment == "anti_tnf" | 
-           Treatment == "pred" |
-           Treatment == "aza" |
-           Treatment == "mtx"
-         )
-
-
-diagnosis_no_treatment <- all_diagnosis %>% 
-  filter(Treatment == "no_biologics" |
-           Treatment == "non_anti_tnf" |
-           Treatment == "no_syst" |
-           Treatment == "no_pred" |
-           Treatment == "no_aza" |
-           Treatment == "no_mtx"
-  )
-
-
-# Plot by diagnosis ----
-breaks <- seq(from = 0, to = 200, by = 25)
-
-plot1 <- ggplot(diagnosis_treatment, aes(Comparison, fill = diagnosis_class)) +
-  geom_bar() + theme_bw() +
-  geom_text(aes(label = after_stat(count)), stat = "count", hjust = 0.5, position = position_stack(vjust = 0.5), colour = "#808080", size = 5) +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
-    axis.title.x=element_blank(),
-    axis.title.y =element_blank(),
-    title = element_text(size = 24),
-    legend.title = element_text(size = 20),
-    legend.text = element_text(size = 14)
-  ) +
-  scale_y_continuous(breaks = breaks, limits = c(0,150)) +
-  labs(fill= 'Diagnosis',
-       title = "Patients using treatment") +
-  scale_fill_brewer(type = "qual", palette = "Pastel2")
-plot1 
-
-ggsave(plot1, file = "Output_files/Patient_distribution/R_crp/byComparison_byDiagnosis_treatment.pdf", height = 8, width = 15, units = "in", dpi = 300)
-
-
-plot2 <- ggplot(diagnosis_no_treatment, aes(Comparison, fill = diagnosis_class)) +
-  geom_bar() + theme_bw() +
-  geom_text(aes(label = after_stat(count)), stat = "count", hjust = 0.5, position = position_stack(vjust = 0.5), colour = "#808080", size = 5) +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
-    axis.title.x=element_blank(),
-    axis.title.y =element_blank(),
-    title = element_text(size = 24),
-    legend.title = element_text(size = 20),
-    legend.text = element_text(size = 14)
-  ) +
-  scale_y_continuous(breaks = breaks, limits = c(0,150)) +
-  labs(fill= 'Diagnosis',
-       title = "Patients included not using treatments") +
-  scale_fill_brewer(type = "qual", palette = "Pastel2")
-plot2 
-
-ggsave(plot2, file = "Output_files/Patient_distribution/R_crp/byComparison_byDiagnosis_notreatment.pdf", height = 8, width = 15, units = "in", dpi = 300)
+plot_remitters_patients_comparisons 
+ggsave(plot_remitters_patients_comparisons, 
+       file = "Output_files/Patients_distribution/inactive_byComparison_byDiagnosis.png", 
+       height = 8, width = 15, units = "in", dpi = 300)
