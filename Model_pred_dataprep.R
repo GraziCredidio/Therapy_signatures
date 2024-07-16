c40f2b37a8c010f22844834770c50084a0cc56fd
Grazi Credidio <grazi.credidio@gmail.com>
2024-07-16 11:37:45 +0200

    Added coldata and couns for all patients

--- Model_pred_dataprep.R
+++ Model_pred_dataprep.R
@@ -29,39 +29,64 @@ coldata <- coldata %>%
     (Prednisolon == "1")~ "pred",
     (No_syst == "1") ~ "no_syst"))
 
-# Filtering for remitters with CRP < 5
-coldata <- coldata %>% 
-  filter(remission == "R") %>% 
-  filter(crp < 5)
-
 # Transforming categorical columns as factor
 coldata <- coldata %>% 
   mutate(across(c(diagnosis_class, age_group, 
                   sex, bmi_class, pred_vs_noSyst), as.factor))
 
+
 # Matching coldata and counts samples ----
-counts <- counts[,colnames(counts) %in% coldata$sample_id]
-idx <- match(colnames(counts), rownames(coldata)) 
-ord.coldata <- coldata[idx, ]
+# All patients
+counts_all <- counts[,colnames(counts) %in% coldata$sample_id]
+idx <- match(colnames(counts_all), rownames(coldata)) 
+ord.coldata_all <- coldata[idx, ]
 
-all(rownames(ord.coldata) %in% colnames(counts))
-all(colnames(counts) == rownames(ord.coldata))
+all(rownames(ord.coldata_all) %in% colnames(counts_all))
+all(colnames(counts_all) == rownames(ord.coldata_all))
 
-write.table(ord.coldata, "Cleaned_tables/models/pred/EZECohort_coldata_pred.txt", sep = "\t", row.names = TRUE)
+write.table(ord.coldata_all, "Cleaned_tables/models/pred/EZECohort_coldata_pred_allPatients.txt", sep = "\t", row.names = TRUE)
+
+# Inactive disease patients: Filtering for inactive disease patients (R) with CRP < 5
+coldata_inactive <- coldata %>% 
+  filter(remission == "R") %>% 
+  filter(crp < 5)
+
+counts_inactive <- counts[,colnames(counts) %in% coldata_inactive$sample_id]
+idx2 <- match(colnames(counts_inactive), rownames(coldata_inactive)) 
+ord.coldata_inactive <- coldata_inactive[idx2, ]
+
+all(rownames(ord.coldata_inactive) %in% colnames(counts_inactive))
+all(colnames(counts_inactive) == rownames(ord.coldata_inactive))
+
+write.table(ord.coldata_inactive, "Cleaned_tables/models/pred/EZECohort_coldata_pred.txt", sep = "\t", row.names = TRUE)
 
 # Normalizing counts ----
-dds_counts <- DESeqDataSetFromMatrix(countData = counts,
-                                     colData = ord.coldata,
-                                     design = ~ diagnosis_class + sex + bmi_class + age_group +
-                                       pred_vs_noSyst)
+deseq_norm <- function(counts, coldata){
+  dds_counts <- DESeqDataSetFromMatrix(countData = counts,
+                                       colData = coldata,
+                                       design = ~ diagnosis_class + sex + bmi_class + age_group + pred_vs_noSyst)
+  
+  dds_counts <- dds_counts[ rowSums(counts(dds_counts) == 0) < 0.8*ncol(counts), ]
+  dds_counts <- estimateSizeFactors(dds_counts)
+  return(dds_counts)
+}
 
-dds_counts <- dds_counts[ rowSums(counts(dds_counts) == 0) < 0.8*ncol(counts), ]
-dds_counts <- estimateSizeFactors(dds_counts)
-normalized_counts <- counts(dds_counts, normalized = TRUE)
-df_normalized_counts <- as.data.frame(normalized_counts)
+# all patients
+dds_counts <- deseq_norm(counts = counts_all, coldata = ord.coldata_all)
+norm_counts <- counts(dds_counts, normalized = TRUE)
+df_normalized_counts <- as.data.frame(norm_counts)
+write.table(df_normalized_counts, "Cleaned_tables/models/pred/normalized_counts_pred_allPatients.txt", sep = "\t",  quote = FALSE)
 
-vst <- vst(dds_counts, blind=TRUE) 
-vst_counts_norm <- assay(vst)
+vst_counts <- vst(dds_counts, blind=TRUE) 
+vst_counts <- assay(vst_counts)
+write.table(vst_counts, "Cleaned_tables/models/pred/vst_counts_pred_allPatients.txt", sep = "\t",  quote = FALSE)
 
-write.table(df_normalized_counts, "Cleaned_tables/models/pred/normalized_counts_pred.txt", sep = "\t",  quote = FALSE)
-write.table(vst_counts_norm, "Cleaned_tables/models/pred/vst_counts_pred.txt", sep = "\t",  quote = FALSE)
+# inactive patients
+dds_counts_inactive <- deseq_norm(counts = counts_inactive, coldata = ord.coldata_inactive)
+norm_counts_inactive <- counts(dds_counts_inactive, normalized = TRUE)
+df_normalized_counts_inactive <- as.data.frame(norm_counts_inactive)
+write.table(df_normalized_counts_inactive, "Cleaned_tables/models/pred/normalized_counts_pred.txt", sep = "\t",  quote = FALSE)
+
+vst_counts_inactive <- vst(dds_counts_inactive, blind=TRUE) 
+vst_counts_inactive <- assay(vst_counts_inactive)
+write.table(vst_counts_inactive, "Cleaned_tables/models/pred/vst_counts_pred.txt", sep = "\t",  quote = FALSE)
