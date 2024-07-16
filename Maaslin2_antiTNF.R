7c25314bfaecf47abcfc3f9a5f442f763c3f5ce7
Grazi Credidio <grazi.credidio@gmail.com>
2024-07-16 13:56:13 +0200

    Added all patients modeling

--- Maaslin2_antiTNF.R
+++ Maaslin2_antiTNF.R
@@ -1,6 +1,7 @@
 # EZE cohort: Therapy Signatures
   # Linear Mixed Model
   # AntiTNF vs No Biologics
+  # Inactive and all patients models
   # Author: Graziella Credidio
 
 rm(list = ls())
@@ -10,34 +11,60 @@ library(tidyverse)
 library(Maaslin2)
 
 # Loading data ----
+# Inactive disease patients
 coldata_noBio <- read.table("Cleaned_tables/models/antiTNF/EZECohort_coldata_antiTNF.txt", sep = "\t")
 vst_noBio <- read.table("Cleaned_tables/models/antiTNF/vst_counts_antiTNF.txt", sep = "\t")
 
-# Setting no_biologics as the reference value ----
-coldata_noBio$antiTNF_vs_noBiologics_mod = coldata_noBio$antiTNF_vs_noBiologics
-coldata_noBio$antiTNF_vs_noBiologics_mod[coldata_noBio$antiTNF_vs_noBiologics_mod == "no_biologics"] =
-  "a_noBiologics"
-coldata_noBio$antiTNF_vs_noBiologics_mod[coldata_noBio$antiTNF_vs_noBiologics_mod == "anti_tnf"] =
-  "z_antiTnf"
+# All patients
+all_coldata_noBio <- read.table("Cleaned_tables/models/antiTNF/EZECohort_coldata_antiTNF_allPatients.txt", sep = "\t")
+all_vst_noBio <- read.table("Cleaned_tables/models/antiTNF/vst_counts_antiTNF_allPatients.txt", sep = "\t")
 
-coldata_noBio <- coldata_noBio %>% 
-  mutate(across(c(diagnosis_class, age_group, sex, bmi_class, Prednisolon, antiTNF_vs_noBiologics_mod), as.factor))
+# Coldata preprocessing ----
+preprocessing_coldata <- function(coldata){
+  # Setting reference values
+  coldata$antiTNF_vs_noBiologics_mod = coldata$antiTNF_vs_noBiologics
+  coldata$antiTNF_vs_noBiologics_mod[coldata$antiTNF_vs_noBiologics_mod == "no_biologics"] = "a_noBiologics"
+  coldata$antiTNF_vs_noBiologics_mod[coldata$antiTNF_vs_noBiologics_mod == "anti_tnf"] = "z_antiTnf"
+  
+  coldata <- coldata %>% 
+    mutate(across(c(diagnosis_class, age_group, sex, 
+                    bmi_class, Prednisolon, antiTNF_vs_noBiologics_mod), as.factor))
+  
+  return(coldata)
+}
 
-dir.create("Output_files/Maaslin2/antiTNF_vs_noBiologics")
+coldata_noBio <- preprocessing_coldata(coldata_noBio)  
+all_oldata_noBio <- preprocessing_coldata(all_coldata_noBio) 
 
-fit_data_noBio = Maaslin2( 
-  input_data = vst_noBio, 
-  input_metadata = coldata_noBio, 
-  analysis_method = "LM",
-  normalization = "NONE",
-  transform = "NONE",
-  output = "Output_files/Maaslin2/antiTNF_vs_noBiologics", 
-  fixed_effects = c("antiTNF_vs_noBiologics_mod", "crp_log", "sex", "Prednisolon"),
-  random_effects = c("diagnosis_class", "age_group", "bmi_class"),
-  reference = c("antiTNF_vs_noBiologics,no_biologics"))
+# Creating output directories ----
+dir.create("Output_files/Maaslin2/antiTNF_vs_noBio/inactive")
+dir.create("Output_files/Maaslin2/antiTNF_vs_noBio/allPatients")
 
-maaslin2_all_results_noBio <- fit_data_noBio$results
-maaslin2_results_noBio <- maaslin2_all_results_noBio %>% filter(metadata == 'antiTNF_vs_noBiologics_mod')
-maaslin2_results_noBio$qval <- p.adjust(maaslin2_results_noBio$pval, method = 'BH')
+# Maaslin2 model ----
+fit_model_antiTNF <- function(coldata, vst_counts, outputPath, resultsPath){
+  model = Maaslin2( 
+    input_data = vst_counts, 
+    input_metadata = coldata, 
+    analysis_method = "LM",
+    normalization = "NONE",
+    transform = "NONE",
+    output = outputPath, 
+    fixed_effects = c("antiTNF_vs_noBiologics_mod", "crp_log", "sex", "Prednisolon"),
+    random_effects = c("diagnosis_class", "age_group", "bmi_class"),
+    reference = c("antiTNF_vs_noBiologics,no_biologics"))
+  
+  # Generating tables
+  maaslin2_all_results_noBio <- model$results
+  maaslin2_all_results_noBio <- maaslin2_all_results_noBio %>% filter(metadata == 'antiTNF_vs_noBiologics_mod') 
+  maaslin2_all_results_noBio$qval <- p.adjust(maaslin2_all_results_noBio$pval, method = 'BH')
+  
+  write.table(maaslin2_all_results_noBio, resultsPath, sep = "\t",  quote = FALSE)
+}
 
-write.table(maaslin2_results_noBio, "Output_files/Maaslin2/maaslin2_results_antiTNF_vs_noBio.txt", sep = "\t",  quote = FALSE)
+fit_model_antiTNF(coldata = coldata_noBio, vst_counts = vst_noBio, 
+              outputPath = "Output_files/Maaslin2/antiTNF_vs_noBiologics/inactive",
+              resultsPath = "Output_files/Maaslin2/results/inactive/maaslin2_results_antiTNF_vs_noBio.txt")
+
+fit_model_antiTNF(coldata = all_coldata_noBio, vst_counts = all_vst_noBio, 
+              outputPath = "Output_files/Maaslin2/antiTNF_vs_noBiologics/allPatients",
+              resultsPath = "Output_files/Maaslin2/results/allPatients/maaslin2_results_antiTNF_vs_noBio_allPatients.txt")
