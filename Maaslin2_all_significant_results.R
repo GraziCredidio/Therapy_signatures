9168d5ed72e2b3ae464bb6329513c828ecf76512
Grazi Credidio <grazi.credidio@gmail.com>
2024-07-16 17:03:07 +0200

    Added all patients models to be filtered to significant genes

--- Maaslin2_all_significant_results.R
+++ Maaslin2_all_significant_results.R
@@ -1,9 +1,11 @@
 # EZE cohort: Therapy Signatures
-  # Linear Mixed Model: filtering significant genes
+  # Linear Mixed Models: filtering significant genes
+  # Inactive disease patients and all patients models
   # Author: Graziella Credidio
 
 rm(list = ls())
 
+# Loading packages ----
 library(tidyverse)
 
 # Loading files ----
@@ -11,18 +13,30 @@ ensg2gene <- read.table("Cleaned_tables/ensg2gene_EZE.txt", header = T, sep = ",
 unique_ensg2gene <- subset(ensg2gene, duplicated(ensg2gene$ensembl_gene_id) == FALSE)
 rownames(unique_ensg2gene) <- unique_ensg2gene$ensembl_gene_id
 
-maaslin_analysis_R_crp <- c("antiTNF_vs_noBio", "aza_vs_noSyst", "pred_vs_noSyst")
+maaslin_analysis_inactive <- c("antiTNF_vs_noBio", "aza_vs_noSyst", "pred_vs_noSyst")
+maaslin_analysis_all <- c("antiTNF_vs_noBio_allPatients", "aza_vs_noSyst_allPatients", "pred_vs_noSyst_allPatients")
 
-
-for (i in maaslin_analysis_R_crp) {
-  
-  comparisons_result <- read.table(file.path(paste("Output_files/Maaslin2/maaslin2_results_", 
-                                                   i, ".txt", sep = "")), sep = "\t")
-  
-  sig_results <- subset(comparisons_result, comparisons_result$qval <= 0.05, drop = FALSE) 
-  sig_results <- sig_results[order(sig_results$qval),]
-  sig_results$genes <- unique_ensg2gene[sig_results$feature, ]$hgnc_symbol
-  
-  write.table(sig_results, file = file.path(paste("Output_files/Maaslin2/significant_results/maaslin2_significant_results_",
-                                                  i, ".txt", sep = "")), sep = "\t", quote = FALSE)
+# Extraction of significant genes from maaslin2 results
+significant_genes <- function(analysis, source_table_path, output_table_path){
+  for (i in analysis) {
+    comparisons_result <- read.table(file.path(paste(source_table_path, 
+                                                     i, ".txt", sep = "")), sep = "\t")
+    
+    sig_results <- subset(comparisons_result, comparisons_result$qval <= 0.05, drop = FALSE) 
+    sig_results <- sig_results[order(sig_results$qval),]
+    sig_results$genes <- unique_ensg2gene[sig_results$feature, ]$hgnc_symbol
+    
+    write.table(sig_results, file = file.path(paste(output_table_path,
+                                                    i, ".txt", sep = "")), sep = "\t", quote = FALSE)
+  }
 }
+
+# inactive disease patients
+significant_genes(maaslin_analysis_inactive, 
+                  "Output_files/Maaslin2/results/inactive/maaslin2_results_",
+                  "Output_files/Maaslin2/significant_results/maaslin2_significant_results_")
+
+# all patients
+significant_genes(maaslin_analysis_all, 
+                  "Output_files/Maaslin2/results/allPatients/maaslin2_results_",
+                  "Output_files/Maaslin2/significant_results/allPatients/maaslin2_allPatients_significant_results_")
